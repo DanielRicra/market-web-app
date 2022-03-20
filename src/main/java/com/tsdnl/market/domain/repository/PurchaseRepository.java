8c4b7a6d93b6833481418a68a93072157776477a
TSdnl13 <daniel.int@outlook.com>
2022-03-19 21:25:53 -0500

    small error,class fixed

--- src/main/java/com/tsdnl/market/domain/repository/PurchaseRepository.java
+++ src/main/java/com/tsdnl/market/domain/repository/PurchaseRepository.java
@@ -1,12 +1,13 @@
 package com.tsdnl.market.domain.repository;
 
+import com.tsdnl.market.domain.DomainPurchase;
 import com.tsdnl.market.domain.PurchaseItem;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface PurchaseRepository {
-    List<PurchaseItem> getAll();
-    Optional<List<PurchaseItem>> getByCustomer(String customerId);
-    PurchaseItem save(PurchaseItem purchase);
+    List<DomainPurchase> getAll();
+    Optional<List<DomainPurchase>> getByCustomer(String customerId);
+    DomainPurchase save(DomainPurchase purchase);
 }
