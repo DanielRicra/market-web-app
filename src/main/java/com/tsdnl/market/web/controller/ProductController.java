07d22eb5869b18ed53f20b7b735ac461d026df0b
TSdnl13 <daniel.int@outlook.com>
2022-03-20 11:44:38 -0500

    starting whit swagger

--- src/main/java/com/tsdnl/market/web/controller/ProductController.java
+++ src/main/java/com/tsdnl/market/web/controller/ProductController.java
@@ -2,6 +2,10 @@ package com.tsdnl.market.web.controller;
 
 import com.tsdnl.market.domain.DomainProduct;
 import com.tsdnl.market.domain.service.ProductService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,12 +21,22 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("")
+    @ApiOperation("Get all supermarket products")
+    @ApiResponse(code = 200, message = "OK")
     public ResponseEntity<List<DomainProduct>> getAll() {
         return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<DomainProduct> getProduct(@PathVariable("id") int productId) {
+    @ApiOperation("Search a product by its ID")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 400, message = "Product not found")
+    })
+    public ResponseEntity<DomainProduct> getProduct(@ApiParam(value = "The id of the product",
+                                                                required = true,
+                                                                example = "7")
+                                                    @PathVariable("id") int productId) {
         return productService.getProduct(productId)
                 .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
